1176154d2523804fb87fb3fdc6a279b144c8d6a8
MrDraMaster <mrdramaster@web.de>
2019-03-25 15:36:32 +0100

    Initial commit

---
+++ BTZ-system.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: BTZ System
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: gerla
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
+
+echo "Hello, World!";
