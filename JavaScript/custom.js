21b0ca5e91fec73d779f14393d67e9a4106607c4
MrDraMaster <mrdramaster@web.de>
2019-03-27 12:42:14 +0100

    added CSS and JavaScript files

---
+++ JavaScript/custom.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function($){
+    // alle class="hidden_name" Elemente verstecken
+    $(".hidden_name").hide();
+    
+    // wenn Kategorie sich ändert:
+    $('#Kategorie').change( function(){
+        var value = this.value;
+        
+        switch(value)
+        {
+            case "AEAP": // AEAP aufdecken
+                $('#Eingabe_AEAP').show();
+                $('#Eingabe_Mitarbeiter').hide();
+                $('#Eingabe_Teilnehmer').hide();
+                break;
+            case "Mitarbeiter": // Mitarbeiter aufdecken
+                $('#Eingabe_Mitarbeiter').show();
+                $('#Eingabe_AEAP').hide();
+                $('#Eingabe_Teilnehmer').hide();                
+				break;
+            case "Teilnehmer": // Teilnehmer aufdecken
+                $('#Eingabe_Teilnehmer').show();
+                $('#Eingabe_Mitarbeiter').hide();
+                $('#Eingabe_AEAP').hide();
+                break;
+            default: // wenn nichts ausgewählt, alle verstecken
+                $('.hidden_name').hide();
+        }
+        
+    })
+});
